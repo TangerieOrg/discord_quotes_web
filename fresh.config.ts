50447535a5d01a994f8f8a7a243304c6680f6b9c
Tangerie <josh.cavill01@gmail.com>
2025-06-27 16:22:27 +1000

    c

--- fresh.config.ts
+++ fresh.config.ts
@@ -4,7 +4,7 @@ import tailwind from "$fresh/plugins/tailwind.ts";
 export default defineConfig({
   plugins: [tailwind()],
   router: {
-    // basePath: Deno.env.get("BASE_URL"),
+    basePath: Deno.env.get("BASE_URL"),
     trailingSlash: true
   },
   server: {
